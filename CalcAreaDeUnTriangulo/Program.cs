6b6de971b60cad8dc93f336f90a209463dd51782
Cristhian De Castro <cristhiancastro01@gmail.com>
2020-08-31 22:30:18 -0400

    Agregar archivos de proyecto.

---
+++ CalcAreaDeUnTriangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CalcAreaDeUnTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la base de el triangulo: ");
+            string valbase = Console.ReadLine();
+            int base1 = Convert.ToInt32(valbase);
+
+            Console.WriteLine("Ingrese la altura de el triangulo: ");
+            string valaltura = Console.ReadLine();
+            int altura = Convert.ToInt32(valaltura);
+
+            Console.WriteLine("El area del triangulo es :{0}", (base1 * altura) / 2);
+  
+        }
+    }
+}
